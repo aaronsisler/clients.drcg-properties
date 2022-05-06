301f32cc7501f5e782fc91c6adaec891ad55b412
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2022-05-06 13:34:50 -0400

    Updating the workflow buttons to disable previous on the first step

--- src/components/workflow-buttons/workflow-buttons.jsx
+++ src/components/workflow-buttons/workflow-buttons.jsx
@@ -2,23 +2,30 @@ import React from "react";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 
-import { useAppDispatch } from "../../state-management";
-import { decrementCurrentStep } from "../../state-management/workflow-slice";
+import { useAppDispatch, useAppSelector } from "../../state-management";
+import {
+  decrementCurrentStep,
+  selectCurrentStep,
+} from "../../state-management/workflow-slice";
 
 import styles from "./workflow-buttons.module.scss";
 
 const WorkflowButtons = ({ goToNextStep }) => {
   const dispatch = useAppDispatch();
+  const currentStep = useAppSelector(selectCurrentStep);
+  const enablePrevious = currentStep > 0;
 
   return (
     <Stack spacing={4} direction="row" className={styles.workflowButtons}>
-      <Button
-        variant="outlined"
-        type="submit"
-        onClick={() => dispatch(decrementCurrentStep())}
-      >
-        Previous
-      </Button>
+      {enablePrevious && (
+        <Button
+          variant="outlined"
+          type="submit"
+          onClick={() => dispatch(decrementCurrentStep())}
+        >
+          Previous
+        </Button>
+      )}
       {goToNextStep && (
         <Button variant="outlined" type="submit" onClick={goToNextStep}>
           Next
