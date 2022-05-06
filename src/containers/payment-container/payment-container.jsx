48ac0407c1e964313fc23870664770270472b94a
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2022-05-05 22:15:12 -0400

    Updating for desktop padding and h1 title of client

--- src/containers/payment-container/payment-container.jsx
+++ src/containers/payment-container/payment-container.jsx
@@ -4,7 +4,7 @@ import { CustomerAddressInfo } from "../../components/customer-address-info";
 import { CustomerContactInfo } from "../../components/customer-contact-info";
 import { PaymentForm } from "../../components/payment-form";
 import { PaymentSuccess } from "../../components/payment-success";
-
+import { CLIENT_NAME } from "../../config";
 import { useAppSelector } from "../../state-management";
 import {
   selectCurrentStep,
@@ -18,6 +18,10 @@ const PaymentContainer = () => {
 
   return (
     <div className={styles.paymentContainer}>
+      <h1 className={styles.paymentContainer__title}>
+        {CLIENT_NAME}&nbsp;Payment&nbsp;Portal
+      </h1>
+      <hr />
       {(() => {
         switch (currentStep) {
           case WorkflowStep.CUSTOMER_CONTACT_INFO:
